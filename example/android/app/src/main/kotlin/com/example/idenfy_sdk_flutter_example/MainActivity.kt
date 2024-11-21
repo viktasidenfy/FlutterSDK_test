f25b283224a3079e40594f7605aa2feaa179a948
viktasidenfy <78145743+viktasidenfy@users.noreply.github.com>
2024-11-21 07:45:22 +0200

    Delete example directory

--- example/android/app/src/main/kotlin/com/example/idenfy_sdk_flutter_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.idenfy_sdk_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const String BASE_URL = 'ivs.idenfy.com';
-const String clientId = 'idenfySampleClientID';
-const String apiKey = 'PUT_YOUR_IDENFY_API_KEY_HERE';
-const String apiSecret = 'PUT_YOUR_IDENFY_API_SECRET_HERE';
